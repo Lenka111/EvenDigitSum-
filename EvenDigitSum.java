b631626cd4f87ca765dc083ebcc39ce7c539e91b
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-02-17 22:20:19 -0600

    Add files via upload

---
+++ EvenDigitSum.java
@@ -0,0 +1,27 @@
+//Elena Voinu
+public class EvenDigitSum {
+    // write your code here
+    public static int getEvenDigitSum(int number){
+        int sum = 0;
+        int lastDigit = 0;
+        if(number < 0)
+            return -1;
+
+        while(number > 0){
+            lastDigit = number % 10;
+            number /=10;
+            
+            if(lastDigit % 2 ==0)
+
+                sum +=lastDigit;
+
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(456));
+    }
+}
+
